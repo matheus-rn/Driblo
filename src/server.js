db08befe6b41befa60e0e96cee61f14b79318ddb
matheus-rn <matheus.1nascimento58@gmail.com>
2019-10-18 22:33:46 -0300

    Configurando nodemon e sucrase
    Co-authored-by: Henrike100 <henrikem100@gmail.com>
    Co-authored-by: rafaelteodosio <rafzteodosio@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,13 +1,5 @@
-const express = require("express");
+import app from "./app";
 
-// Constants
 const PORT = 8001;
-const HOST = "0.0.0.0";
 
-// App
-const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello world\n");
-});
-
-app.listen(PORT, HOST);
+app.listen(PORT);
